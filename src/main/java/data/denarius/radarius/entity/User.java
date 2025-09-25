f3011bd049f200f7febb8399ad687b7bd13f2a73
DaviSFS21 <davisfs2110@gmail.com>
2025-09-25 16:02:13 -0300

    feat: start security implementation and add password attribute

--- src/main/java/data/denarius/radarius/entity/User.java
+++ src/main/java/data/denarius/radarius/entity/User.java
@@ -1,13 +1,18 @@
 package data.denarius.radarius.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.OffsetDateTime;
-import java.util.List;
 
 @Entity
 @Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
@@ -18,56 +23,10 @@ public class User {
 
     private String email;
 
+    private String password;
+
     private String role;
 
     @Column(name = "created_at")
     private OffsetDateTime createdAt;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getWhatsapp() {
-        return whatsapp;
-    }
-
-    public void setWhatsapp(String whatsapp) {
-        this.whatsapp = whatsapp;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(OffsetDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
---
+++ src/main/java/data/denarius/radarius/security/JwtToPrincipalConverter.java
@@ -0,0 +1,14 @@
+package data.denarius.radarius.security;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtToPrincipalConverter {
+    public static UserPrincipal convert(DecodedJWT jwt) {
+        return UserPrincipal.builder()
+                .userId(Integer.getInteger(jwt.getSubject()))
+                .email(jwt.getClaim("e").asString())
+                .build();
+    }
+}
