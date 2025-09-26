32cb78143154f9354e0be24a0a2abdb0ad649e38
DaviSFS21 <davisfs2110@gmail.com>
2025-09-25 22:46:32 -0300

    feat: exception treatment and enumerator for roles

--- src/main/java/data/denarius/radarius/service/AuthService.java
+++ src/main/java/data/denarius/radarius/service/AuthService.java
@@ -2,6 +2,8 @@ package data.denarius.radarius.service;
 
 import data.denarius.radarius.dto.login.LoginResponseDTO;
 
+import javax.naming.AuthenticationException;
+
 public interface AuthService {
-    LoginResponseDTO login(String email, String password);
+    LoginResponseDTO login(String email, String password) throws AuthenticationException;
 }
