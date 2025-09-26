b20d8529f0407134288b69f94127ceaccc702dde
DaviSFS21 <davisfs2110@gmail.com>
2025-09-25 22:46:01 -0300

    fix: correct classes crashing because of constructors missing

--- src/main/java/data/denarius/radarius/service/impl/AuthServiceImpl.java
+++ src/main/java/data/denarius/radarius/service/impl/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import data.denarius.radarius.dto.login.LoginResponseDTO;
 import data.denarius.radarius.security.JwtIssuer;
 import data.denarius.radarius.security.UserPrincipal;
 import data.denarius.radarius.service.AuthService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,7 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private JwtIssuer jwtIssuer;
     private final AuthenticationManager authenticationManager;
@@ -22,7 +23,7 @@ public class AuthServiceImpl implements AuthService {
         );
         SecurityContextHolder.getContext().setAuthentication(auth);
         var userPrincipal = (UserPrincipal)auth.getPrincipal();
-    var token = jwtIssuer.issue(userPrincipal.getUserId(), userPrincipal.getEmail());
+        var token = jwtIssuer.issue(userPrincipal.getUserId(), userPrincipal.getEmail());
         return new LoginResponseDTO(token);
     }
 }
