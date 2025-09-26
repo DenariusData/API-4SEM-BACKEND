34ea24778a78898f3586a26c79641072aa183bb0
DaviSFS21 <davisfs2110@gmail.com>
2025-09-25 22:46:32 -0300

    feat: exception treatment and enumerator for roles

--- src/main/java/data/denarius/radarius/controllers/AuthController.java
+++ src/main/java/data/denarius/radarius/controllers/AuthController.java
@@ -2,6 +2,7 @@ package data.denarius.radarius.controllers;
 
 import data.denarius.radarius.dto.login.LoginRequestDTO;
 import data.denarius.radarius.dto.login.LoginResponseDTO;
+import data.denarius.radarius.exceptions.AuthorizationException;
 import data.denarius.radarius.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.AuthenticationException;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -17,7 +20,7 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto) throws AuthenticationException {
         return ResponseEntity.ok(authService.login(dto.email(), dto.password()));
     }
 }
