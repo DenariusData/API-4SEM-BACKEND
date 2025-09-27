86a34d644d3391d828948d3338a2edeac23e4f2d
joaopaulista <jvpaulistaa@gmail.com>
2025-09-27 15:08:44 -0300

    feat: add Reading DTO

---
+++ src/main/java/data/denarius/radarius/dtos/ReadingRequestDTO.java
@@ -0,0 +1,30 @@
+package data.denarius.radarius.dto;
+
+import data.denarius.radarius.enums.VehicleTypeEnum;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class ReadingRequestDTO {
+
+    private Integer cameraId;
+    private OffsetDateTime timestamp;
+    private VehicleTypeEnum vehicleType;
+    private BigDecimal speed;
+    private String plate;
+
+    // Getters e Setters
+    public Integer getCameraId() { return cameraId; }
+    public void setCameraId(Integer cameraId) { this.cameraId = cameraId; }
+
+    public OffsetDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(OffsetDateTime timestamp) { this.timestamp = timestamp; }
+
+    public VehicleTypeEnum getVehicleType() { return vehicleType; }
+    public void setVehicleType(VehicleTypeEnum vehicleType) { this.vehicleType = vehicleType; }
+
+    public BigDecimal getSpeed() { return speed; }
+    public void setSpeed(BigDecimal speed) { this.speed = speed; }
+
+    public String getPlate() { return plate; }
+    public void setPlate(String plate) { this.plate = plate; }
+}
