b5bfd7f5238f9930427c0acb31f7b8a16c66fa64
DaviSFS21 <davisfs2110@gmail.com>
2025-10-09 11:22:04 -0300

    chore: add SQL and PL/SQL commands to codebase

---
+++ src/main/resources/database/drop-all-tables.sql
@@ -0,0 +1,11 @@
+DECLARE
+  v_table_name VARCHAR2(100);
+  v_user_name VARCHAR2(100);
+BEGIN
+  FOR c IN (SELECT owner, table_name FROM all_tables WHERE owner LIKE 'DEN_D%') LOOP
+    v_table_name := c.table_name;
+    v_user_name := c.owner;
+    EXECUTE IMMEDIATE 'DROP TABLE "' || v_user_name || '"."' || v_table_name || '" CASCADE CONSTRAINTS';
+  END LOOP;
+END;
+/
