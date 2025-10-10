9d08fd45de3e085ee240146bc12e24bedc3bfb7a
joaopaulista <jvpaulistaa@gmail.com>
2025-10-09 21:36:18 -0300

    feat: add controller, service, impl and repository for Region entity

---
+++ src/main/java/data/denarius/radarius/dto/region/RegionResponseDTO.java
@@ -0,0 +1,23 @@
+package data.denarius.radarius.dto.region;
+
+import java.time.LocalDateTime;
+
+public class RegionResponseDTO {
+    private Integer id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Getters e Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+    public void setUpdatedAt(LocalDateTime updatedAt) { this.updatedAt = updatedAt; }
+}
