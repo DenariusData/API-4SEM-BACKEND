9ce5c609f07efc64f52366dffa0023fa4486bd69
Tiago <ttr100ttr100@gmail.com>
2025-10-11 10:11:49 -0300

    feat: add AlertRepository with filtering and pagination support

--- src/main/java/data/denarius/radarius/repository/AlertRepository.java
+++ src/main/java/data/denarius/radarius/repository/AlertRepository.java
@@ -1,9 +1,43 @@
 package data.denarius.radarius.repository;
 
 import data.denarius.radarius.entity.Alert;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, Integer> {
+
+    @Query("""
+        SELECT a
+        FROM Alert a
+        WHERE a.camera.region.id = :regionId
+        ORDER BY a.createdAt DESC
+        """)
+    List<Alert> findTop10ByRegion(@Param("regionId") Integer regionId, Pageable pageable);
+
+    @Query("""
+        SELECT a
+        FROM Alert a
+        WHERE (:regionId IS NULL OR a.camera.region.id = :regionId)
+        AND (:cameraId IS NULL OR a.camera.id = :cameraId)
+        AND (:startDate IS NULL OR a.createdAt >= :startDate)
+        AND (:endDate IS NULL OR a.createdAt <= :endDate)
+        ORDER BY a.createdAt DESC
+        """)
+    Page<Alert> findWithFilters(
+            @Param("regionId") Integer regionId,
+            @Param("cameraId") Integer cameraId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
+
 }
