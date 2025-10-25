8d3d8b24d7cc6c7bb9d1fbae2534ddae87f6e15a
augustopiatto <ampiatto@hotmail.com>
2025-10-25 14:52:21 -0300

    refactor: remove unused code

--- src/main/java/data/denarius/radarius/service/criterion/CriterionCalculator.java
+++
@@ -1,9 +0,0 @@
-package data.denarius.radarius.service.criterion;
-
-import data.denarius.radarius.dto.CriterionCalculationResult;
-import data.denarius.radarius.entity.Camera;
-
-public interface CriterionCalculator {
-    CriterionCalculationResult calculate(Camera camera);
-    String getCriterionName();
-}
